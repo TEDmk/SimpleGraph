ad5f3ca4cc6b912fb7a538e22749f8cde62f2ad1
TeddyTrqt <turquet.teddy@gmail.com>
2020-05-25 21:13:03 +0200

    Add measure merging

--- src/Scale.ts
+++ src/Scale.ts
@@ -17,14 +17,6 @@ export class TimeScale {
 // TODO: finish
 
 export function normalizeTimeScale(timeScale: TimeScale) {
-    // if(timeScale.deltaPixel > 2 * timeScale.baseDeltaPixel) {
-    //     timeScale.deltaPixel = timeScale.baseDeltaPixel;
-    //     timeScale.deltaSecond = timeScale.deltaSecond / 2;
-    // }
-    // if(timeScale.deltaPixel < timeScale.baseDeltaPixel / 2) {
-    //     timeScale.deltaPixel = timeScale.baseDeltaPixel;
-    //     timeScale.deltaSecond = timeScale.deltaSecond * 2;
-    // }
     if(timeScale.pixelOffset > timeScale.deltaPixel) {
         timeScale.pixelOffset = timeScale.pixelOffset - timeScale.deltaPixel;   
         timeScale.startDate = new Date(timeScale.startDate.getTime() - timeScale.deltaSecond * 1000);
@@ -33,18 +25,19 @@ export function normalizeTimeScale(timeScale: TimeScale) {
         timeScale.pixelOffset = timeScale.pixelOffset + timeScale.deltaPixel;
         timeScale.startDate = new Date(timeScale.startDate.getTime() + timeScale.deltaSecond * 1000);
     }
+
+    if(timeScale.deltaPixel >= 2 * timeScale.baseDeltaPixel) {
+        timeScale.deltaPixel = timeScale.baseDeltaPixel;
+        timeScale.deltaSecond = timeScale.deltaSecond / 2;
+    }
+    if(timeScale.deltaPixel <= timeScale.baseDeltaPixel / 2) {
+        timeScale.deltaPixel = timeScale.baseDeltaPixel;
+        timeScale.deltaSecond = timeScale.deltaSecond * 2;
+    }
     return timeScale
 }
 
 export function normalizeDataScale(dataScale: DataScale) {
-    // if(dataScale.deltaPixel > 2 * dataScale.baseDeltaPixel) {
-    //     dataScale.deltaValue = dataScale.deltaValue * (dataScale.baseDeltaPixel / dataScale.deltaPixel);
-    //     dataScale.deltaPixel = dataScale.baseDeltaPixel;
-    // }
-    // if(dataScale.deltaPixel < dataScale.baseDeltaPixel / 2) {
-    //     dataScale.deltaPixel = dataScale.baseDeltaPixel;
-    //     dataScale.deltaValue = dataScale.deltaValue / (dataScale.baseDeltaPixel / dataScale.deltaPixel);
-    // }
     if(dataScale.pixelOffset > dataScale.deltaPixel) {
         dataScale.pixelOffset = dataScale.pixelOffset - dataScale.deltaPixel;   
         dataScale.startValue = dataScale.startValue - dataScale.deltaValue;
@@ -52,6 +45,14 @@ export function normalizeDataScale(dataScale: DataScale) {
     if(dataScale.pixelOffset < -dataScale.deltaPixel) {
         dataScale.pixelOffset = dataScale.pixelOffset + dataScale.deltaPixel;  
         dataScale.startValue = dataScale.startValue + dataScale.deltaValue;
+    }
+    if(dataScale.deltaPixel >= 2 * dataScale.baseDeltaPixel) {
+        dataScale.deltaValue = dataScale.deltaValue / 2;
+        dataScale.deltaPixel = dataScale.baseDeltaPixel;
+    }
+    if(dataScale.deltaPixel <= dataScale.baseDeltaPixel / 2) {
+        dataScale.deltaPixel = dataScale.baseDeltaPixel;
+        dataScale.deltaValue = dataScale.deltaValue * 2;
     }
     return dataScale
 }
